1f1f6274780273c6cca274f772fa3810dec87328
wryyyyyy <1428448282@qq.com>
2026-07-29 09:25:25 -0700

    feat: 封装SqlSessionUtil工具类

--- mybatis-001/src/test/java/com/example/mybatis/MyBatisTest.java
+++ mybatis-001/src/test/java/com/example/mybatis/MyBatisTest.java
@@ -1,5 +1,6 @@
 package com.example.mybatis;
 
+import com.example.mybatis.util.SqlSessionUtil;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -20,6 +21,17 @@ public class MyBatisTest {
                 sqlSession.commit();
             }
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSqlSeesionUtil() {
+        try(SqlSession sqlSession = SqlSessionUtil.openSession()) {
+            int count = sqlSession.insert("insertUser");
+            System.out.println(count == 1 ? "保存成功" : "保存失败");
+            sqlSession.commit();
+        }catch (Exception e) {
             e.printStackTrace();
         }
     }
