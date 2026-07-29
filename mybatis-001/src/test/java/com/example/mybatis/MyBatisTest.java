1e117b01796fda537fede8b1cf0a7fac8b99eda7
wryyyyyy <1428448282@qq.com>
2026-07-29 09:08:28 -0700

    test: 引入JUnit单元测试

---
+++ mybatis-001/src/test/java/com/example/mybatis/MyBatisTest.java
@@ -0,0 +1,26 @@
+package com.example.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+public class MyBatisTest {
+    @Test
+    public void testFirst() {
+        try (InputStream in = Resources.getResourceAsStream("mybatis-config.xml")) {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+
+            try(SqlSession sqlSession = sqlSessionFactory.openSession()) {
+                int count = sqlSession.insert("insertUser");
+                System.out.println(count == 1 ? "保存成功" : "保存失败");
+                sqlSession.commit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
