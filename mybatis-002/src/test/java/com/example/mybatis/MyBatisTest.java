05cfa9e9776a86e8ebf475707417150fa722feb9
wryyyyyy <1428448282@qq.com>
2026-07-30 07:36:07 -0700

    feat: 基于Entity实现保存

--- mybatis-002/src/test/java/com/example/mybatis/MyBatisTest.java
+++ mybatis-002/src/test/java/com/example/mybatis/MyBatisTest.java
@@ -1,5 +1,6 @@
 package com.example.mybatis;
 
+import com.example.mybatis.entity.User;
 import com.example.mybatis.util.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,9 @@ public class MyBatisTest {
     public void testInsertWithMap() {
         Map<String, Object> data = new HashMap<>();
         // k1\k2\k3 对应 UserMapper 中的 k1\k2\k3 占位符
-        data.put("k1", "hjj");
-        data.put("k2", 18);
-        data.put("k3", "男");
+        data.put("name", "hjj");
+        data.put("age", 18);
+        data.put("sex", "男");
 
         try(SqlSession sqlSession = SqlSessionUtil.openSession()) {
 
@@ -22,6 +23,27 @@ public class MyBatisTest {
             System.out.println(count == 1 ? "保存成功" : "保存失败");
 
             sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInsertWithEntity() {
+    //     准备数据
+        User user = new User();
+        user.setAge(5);
+        user.setName("张三");
+        user.setSex("女");
+
+        try(SqlSession sqlSession = SqlSessionUtil.openSession()) {
+
+            int count = sqlSession.insert("insertUser2", user);
+
+            System.out.println(count == 1 ? "保存成功" : "保存失败");
+
+            sqlSession.commit();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
